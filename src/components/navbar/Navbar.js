5149615adb60d0fc4af03addd24f29d16021cd37
Elijahscriptdev <elijahobominuru@gmail.com>
2021-03-12 18:13:43 +0100

    styled navbar for destop

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/Landkit..svg";
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className="nav-items">
-                <ul>
-                    <li>LandKit</li>
-                </ul>
-            </div>
-            <div className="nav-items">
-                <div className="nav-items-links">
-                    <ul>
-                        <li>Landings</li>
-                        <li>Pages</li>
-                        <li>Account</li>
-                        <li>Documentation</li>
-                    </ul>
-                </div>
-                <div className="nav-items-links">
-                    <ul>
-                        <li>Documentation</li>
-                        <li>Buy Now</li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className='nav-container'>
+      <div className='nav-logo'>
+        <ul>
+          <li>
+            <img src={logo} alt='' />
+          </li>
+        </ul>
+      </div>
+      <div className='nav-items'>
+        <div className='nav-items-links'>
+          <ul>
+            <li>Landings</li>
+            <li>Pages</li>
+            <li>Account</li>
+            <li>Documentation</li>
+          </ul>
         </div>
-    )
-}
+        <div className='nav-items-links'>
+          <ul>
+            <li>Documentation</li>
+            <li className='button'>Buy Now</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
